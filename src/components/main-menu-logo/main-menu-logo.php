a0c83a91c7963788978da0943bbf3c579e5065e6
Andy Pearson <andy@londonparkour.com>
2021-04-30 10:15:05 +0100

    aria

--- src/components/main-menu-logo/main-menu-logo.php
+++ src/components/main-menu-logo/main-menu-logo.php
@@ -1,6 +1,6 @@
 <h1 class="flex-1">
     <a class="logo" href="https://londonparkour.com" title="London Parkour">
-        <svg class="h-16 w-32 sm:w-48 fill-white hover:fill-Amber500">
+        <svg role="img" aria-label="London Parkour Logo" class="h-16 w-32 sm:w-48 fill-white hover:fill-Amber500">
             <title>London Parkour</title>
             <desc>Company Logo</desc>
             <use xlink:href="#h-logo-text" />
